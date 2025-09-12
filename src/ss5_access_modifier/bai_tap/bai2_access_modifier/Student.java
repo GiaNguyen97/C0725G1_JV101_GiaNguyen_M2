6cfda06d0d76d406776835d76afa4758b0d33526
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-09-12 10:42:14 +0700

    SS5 bài tập 2

---
+++ src/ss5_access_modifier/bai_tap/bai2_access_modifier/Student.java
@@ -0,0 +1,22 @@
+package ss5_access_modifier.bai_tap.bai2_access_modifier;
+
+public class Student {
+    private String name = "John";
+    private String classes = "C02";
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setClasses(String classes) {
+        this.classes = classes;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", classes='" + classes + '\'' +
+                '}';
+    }
+}
---
+++ src/ss5_access_modifier/bai_tap/bai2_access_modifier/Test.java
@@ -0,0 +1,11 @@
+package ss5_access_modifier.bai_tap.bai2_access_modifier;
+
+public class Test {
+    public static void main(String[] args) {
+        Student student = new Student();
+        System.out.println(student.toString());
+        student.setName("Nguyen");
+        student.setClasses("C07");
+        System.out.println(student.toString());
+    }
+}
