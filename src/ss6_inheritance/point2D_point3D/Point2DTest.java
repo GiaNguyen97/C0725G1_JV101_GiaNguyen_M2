32c86a9959de58b51ef697257aafcf39c726a943
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-09-15 13:52:26 +0700

    Bài tập 2

---
+++ src/ss6_inheritance/point2D_point3D/Point2DTest.java
@@ -0,0 +1,12 @@
+package ss6_inheritance.point2D_point3D;
+
+import java.util.Arrays;
+
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        point2D.setXY(32,24);
+        System.out.println(Arrays.toString(point2D.getXY()));
+        System.out.println(point2D);
+    }
+}
---
+++ src/ss6_inheritance/point2D_point3D/Point3DTest.java
@@ -0,0 +1,12 @@
+package ss6_inheritance.point2D_point3D;
+
+import java.util.Arrays;
+
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        point3D.setXYZ(3,1,4);
+        System.out.println(Arrays.toString(point3D.getXYZ()));
+        System.out.println(point3D);
+    }
+}
