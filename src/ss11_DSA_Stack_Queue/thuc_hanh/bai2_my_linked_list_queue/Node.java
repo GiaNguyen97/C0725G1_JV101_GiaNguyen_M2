09836e355ef0415564c5de4c5d1a8e87db5c6e56
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-09-22 14:22:35 +0700

    ss1 thực hành 2

---
+++ src/ss11_DSA_Stack_Queue/thuc_hanh/bai2_my_linked_list_queue/Node.java
@@ -0,0 +1,11 @@
+package ss11_DSA_Stack_Queue.thuc_hanh.bai2_my_linked_list_queue;
+
+public class Node {
+    public int key;
+    public Node next;
+
+    public Node(int key) {
+        this.key = key;
+        this.next = null;
+    }
+}
