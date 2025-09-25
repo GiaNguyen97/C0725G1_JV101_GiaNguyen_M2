4aeb818cb79b31b48260d313ce7ccbd8f00a506d
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-09-25 09:45:41 +0700

    ss16 thực hành bài 1

---
+++ src/ss9_test_fisrt_TDD/thuc_hanh/absolute_number_calculator/AbsoluteNumberCalculator.java
@@ -0,0 +1,7 @@
+package ss9_test_fisrt_TDD.thuc_hanh.absolute_number_calculator;
+
+public class AbsoluteNumberCalculator {
+    public static int findAbsolute(int number) {
+        throw new UnsupportedOperationException();
+    }
+}
