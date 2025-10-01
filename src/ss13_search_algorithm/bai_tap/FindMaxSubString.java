00b4a0eba7d2d73ca5d0e09cabbddc1c3d068276
GiaNguyen97 <tranlegianguyen97dn@gmail.com>
2025-10-01 15:57:00 +0700

    ss13 bài 3

---
+++ src/ss13_search_algorithm/bai_tap/FindMaxSubString.java
@@ -0,0 +1,32 @@
+package ss13_search_algorithm.bai_tap;
+
+import java.util.Scanner;
+
+public class FindMaxSubString {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Nhập chuỗi: ");
+        String text = scanner.nextLine();
+
+        StringBuilder maxSubsequence = new StringBuilder();
+
+        for (int i = 0; i < text.length(); i++) {
+            StringBuilder subsequence = new StringBuilder();
+            subsequence.append(text.charAt(i));
+            for (int j = i + 1; j < text.length(); j++) {
+                if (subsequence.charAt(subsequence.length() - 1) < text.charAt(j)) {
+                    subsequence.append(text.charAt(j));
+                } else {
+                    break;
+                }
+            }
+            if (maxSubsequence.length() < subsequence.length()) {
+                maxSubsequence.setLength(0);
+                maxSubsequence.append(subsequence);
+            }
+            subsequence.setLength(0);
+        }
+
+        System.out.println("Chuỗi con theo thứ tự tăng dần dài nhất là: " + maxSubsequence);
+    }
+}
